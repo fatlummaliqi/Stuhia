8eeddc1ecd4f4d80ceab380f00a68150370f6b5e
Fatlum Maliqi <fatlum@gjirafa.com>
2024-03-17 20:30:15 +0100

    Add unit test for InternalEventPublisher.PublishAsync

--- src/Stuhia/Core/InternalEventPublisher.cs
+++ src/Stuhia/Core/InternalEventPublisher.cs
@@ -1,5 +1,4 @@
-﻿
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Stuhia.Context;
 
 namespace Stuhia.Core;
---
+++ test/Stuhia.Tests.Unit/Tests/EventPublisherTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using Stuhia.Configurations;
+using Stuhia.Context;
+using Stuhia.Core;
+using Stuhia.Tests.Unit.Helpers.Events;
+using System.Reflection;
+using Xunit;
+
+namespace Stuhia.Tests.Unit.Tests;
+
+public class EventPublisherTests
+{
+    private readonly StuhiaConfiguration _configuration = new()
+    {
+        EnableLogging = false,
+        SilentFailures = false,
+        AssembliesToScan = [ Assembly.GetExecutingAssembly() ]
+    };
+
+    [Fact]
+    public async Task PublishAsync_Should_Complete_Succefully()
+    {
+        //Arrange
+        EventContext.Current.Construct(_configuration);
+        var serviceProvider = Mock.Of<IServiceProvider>();  
+        var eventPublisher = new InternalEventPublisher(serviceProvider, logger: null);
+
+        //Act
+        await eventPublisher.PublishAsync(new PeekabooEvent());
+
+        //Assert
+        Assert.True(true);
+    }
+}
