8c79bb227c71f5841080fc1af44064f5d9a76fa6
Fatlum Maliqi <fatlum@gjirafa.com>
2024-03-17 20:54:00 +0100

    Move configs for assembly internals to Stuhia.csproj

--- src/Stuhia/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Stuhia.Tests.Unit")]
-[assembly: InternalsVisibleTo("Stuhia.Tests.Acceptance")]
-[assembly: InternalsVisibleTo("Stuhia.Tests.Integration")]
